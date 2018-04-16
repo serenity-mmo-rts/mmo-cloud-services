838734ea33be2d78a4e47ea8be6fb41a3e30f2d3
Holger Finger <hfinger@users.noreply.github.com>
2018-04-16 16:32:30 +0200

    Added Linux support

--- src/server/initGameData/initGameTypesTechnologies.js
+++ src/server/initGameData/initGameTypesTechnologies.js
@@ -1,18 +1,6 @@
 var node = !(typeof exports === 'undefined');
 if (node) {
-    var GameData = require('../../game/GameData').GameData;
-    var State = require('../../game/AbstractBlock').State;
-    var MapObject = require('../../game/MapObject').MapObject;
-    var LayerType = require('../../game/types/LayerType').LayerType;
-    var ObjectType = require('../../game/types/objectType').ObjectType;
-    var RessourceType = require('../../game/types/ResourceType').RessourceType;
     var TechnologyType = require('../../game/types/TechnologyType').TechnologyType;
-    var ItemType = require('../../game/types/ItemType').ItemType;
-    var UserType = require('../../game/types/userType').UserType;
-    var Spritesheet = require('../../game/Spritesheet').Spritesheet;
-    var Layer = require('../../game/Layer').Layer;
-    var User = require('../../game/User').User;
-
 }
 
 (function (exports) {
